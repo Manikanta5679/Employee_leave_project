9fdc0627b929b696433f5852e860fba18106d46e
Manikanta5679 <mkkurapati23@gmail.com>
2025-08-12 15:16:06 +0530

    new commit

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -17,20 +17,26 @@ resource "aws_instance" "app_instance" {
   }
 
   user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y docker.io unzip
 
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-              unzip awscliv2.zip
-              ./aws/install
+    systemctl start docker
+    systemctl enable docker
 
-              aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.ecr_repo_url}
-              docker pull ${var.ecr_repo_url}:latest
-              docker stop backend || true
-              docker rm backend || true
-              docker run -d -p ${var.instance_port}:${var.instance_port} --restart unless-stopped --name backend ${var.ecr_repo_url}:latest
-              EOF
+    usermod -aG docker ubuntu
+
+    # Install AWS CLI v2
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    ./aws/install
+
+    # Login to ECR and pull image
+    aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.ecr_repo_url}
+
+    docker pull ${var.ecr_repo_url}:latest
+    docker stop backend || true
+    docker rm backend || true
+    docker run -d -p ${var.instance_port}:${var.instance_port} --restart unless-stopped --name backend ${var.ecr_repo_url}:latest
+  EOF
 }
